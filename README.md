17d992ba1a892f3108a6e0f924166ef2501563e5
Paul Langeslag <planges@uni-goettingen.de>
2026-03-12 16:06:55 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# olg-corpus-tools
+
+Processing tools for Old Low German (i.e. Old Saxon) text corpora and resources.
+
+## `helipad-extract.py` and `helipad-extract.ipynb`
+
+A script extracting token forms with their lemma, POS, and verse ID metadata from the CorpusSearch PSD-formatted [HeliPaD](https://github.com/DiGS-Corpora/HeliPaD) corpus (i.e. the text of [Sievers 1878](https://archive.org/details/heliandherausgvonsieve), representing the C text ([London, British Library, MS Cotton Caligula A. vii](https://searcharchives.bl.uk/catalog/041-001102326)) of the _Heliand_, and outputting them in JSON and plaintext formats. The script has a few variables for tweaking the presentation of the plaintext output.
+
+Requires `GitPython`.
+
+## `helicord.py`
+
+A command-line concordance taking any number of headword and/or inflected forms as arguments, and returning matching halfline references with the lemma or form encountered.
+
+Requires `GitPython` and `prettytable`.
+
+## License Notice
+
+These tools are Copyright 2026 P. S. Langeslag.
+
+These tools are free software: you can redistribute them and/or modify them under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+These tools are distributed in the hope that they will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with these tools. If not, see <https://www.gnu.org/licenses/>.
---
+++ helicord.py
@@ -0,0 +1,83 @@
+# HeliPaD concordance.
+# For lemma forms see HeliPaD, or heliand-c.json.
+import argparse,re,json
+from pathlib import Path
+from git import Repo
+from prettytable import PrettyTable
+
+argparser = argparse.ArgumentParser()
+argparser.add_argument("query", nargs='+')
+args = argparser.parse_args()
+query = args.query
+
+def concord(query):
+    lemma_hits = [i['verse'] + ': ' + i['form'] for i in tokens if i['lemma'] == query]
+    form_hits = [i['verse'] + ': ' + i['lemma'] for i in tokens if i['form'] == query]
+    if len(lemma_hits) == 0:
+        lemma_hits.append('No hits.')
+    if len(form_hits) == 0:
+        form_hits.append('No hits.')
+    if len(lemma_hits) - len(form_hits) > 0:
+        form_hits.extend([''] * (len(lemma_hits) - len(form_hits)))
+    elif len(form_hits) - len(lemma_hits) > 0:
+        lemma_hits.extend([''] * (len(form_hits) - len(lemma_hits)))
+    table = PrettyTable()
+    table.add_column('"' + query + '" as lemma:',lemma_hits)
+    table.add_column('"' + query + '" as form:',form_hits)
+    table.align = 'l'
+    print(table)
+
+json_file = 'heliand-c.json'
+if Path(json_file).is_file():
+    print('heliand-c.json already present. Loading...')
+    with open(json_file) as json_data:
+        tokens = json.load(json_data)
+else:
+    # HTTPS clone point:
+    remote = 'https://github.com/DiGS-Corpora/HeliPaD.git'
+    # Desired target folder name:
+    local = 'HeliPaD'
+    # Only clone if the target folder doesn't already exist:
+    if not(Path(local).is_dir()):
+        print('HeliPaD not found. Cloning...')
+        repo = Repo.clone_from(remote, local)
+    # Else, just update the working copy from remote:
+    else:
+        print('HeliPaD found.')
+        repo = Repo(local)
+        assert isinstance(repo, Repo)
+        repo.remotes.origin.pull()
+    assert not repo.bare
+    
+    with open('HeliPaD/heliand.psd') as infile:
+        psd = infile.read().splitlines()
+    
+    pattern = re.compile(r"\(([A-Z0-9^+=*$-]*)\s(\w*)-([^)]*)\)")
+    line_boundary = re.compile(r"\(CODE <R_(\d*)")
+    caesura = re.compile(r"\(CODE <C>")
+    
+    print('Generating heliand-c.json...')
+    tokens = []
+    line_num = 1
+    halfline = 'a'
+    for line in psd:
+        token = dict()
+        token['verse'] = str(line_num) + halfline
+        result = pattern.search(line)
+        newline = line_boundary.search(line)
+        off_verse = caesura.search(line)
+        if result:
+            token['form'] = result.group(2)
+            token['lemma'] = result.group(3)
+            token['pos'] = result.group(1)
+            tokens.append(token)
+        elif off_verse:
+            halfline = 'b'
+        elif newline:
+            line_num = int(newline.group(1))
+            halfline = 'a'
+    with open(json_file, 'w', encoding='utf-8') as outfile:
+        json.dump(tokens, outfile, ensure_ascii=False, indent=4)
+
+for subquery in query:
+    concord(subquery)
