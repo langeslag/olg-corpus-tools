b66d9ba519a4065a55490b3c4558a88b0efdeb59
Paul Langeslag <planges@uni-goettingen.de>
2026-05-28 07:32:31 +0200

    Worked on identify_lifts.py

--- identify_lifts.py
+++ identify_lifts.py
@@ -1,9 +1,17 @@
+# Attempt to identify the lifts in Old Low German verse.
 import json
 from pathlib import Path
+
+c_range = 5969
+c_json_file = Path('heliand-c.json')
+c_verses_file = Path('heliand-c-verses.json')
+json_out = Path('heliand-c-scansion.json')
+plaintext_out = Path('heliand-c-lifts.txt')
 
 stress_words = [
         'N',
         'NR',
+        'NPR',
         'ADJ',
         'ADJR',
         'ADJS',
@@ -11,6 +19,7 @@ stress_words = [
         'VB',
         'VAG',
         'VBN',
+        'VN',
         'BE',
         'BAG',
         'BEN',
@@ -27,14 +36,17 @@ stress_words = [
         'QS'
         ]
 
-particles = [
-        'PRO',
-        'PRO$',
-        'MAN',
-        'C',
+stress_words_or_particles = [
         'ADV',
         'ADR',
         'ADS',
+        'PRO',
+        'PRO$'
+        ]
+
+particles = [
+        'MAN',
+        'C',
         'VBI',
         'VBPH',
         'VBPI',
@@ -77,30 +89,55 @@ particles = [
 proclitics = [
         'D',
         'P',
+        'CONJ',
         'NEG'
         ]
 
-c_range = 5969
-c_json_file = Path('heliand-c.json')
+exceptionally_proclitics = [
+        'sulic'
+        ]
 
 def scan(line):
+    data = []
     formatted = []
-    if line['a'] is not None and line['b'] is not None:
-        long_line = line['a'] + line['b']
-        for token in long_line:
-            if token['pos'].split('^')[0] in stress_words:
+    line_type = ''
+    line_tokens = []
+    line_no = str(0)
+    if line['a'] is not None and len(line['a']) > 0:
+        line_type = 'a'
+        line_no = line['a'][0]['verse'].rstrip('ax')
+    if line['b'] is not None and len(line['b']) > 0:
+        line_type = line_type + 'b'
+        line_no = line['b'][0]['verse'].rstrip('bx')
+    for halfline in line_type:
+        for idx,token in enumerate(line[halfline]):
+            token['lift'] = None
+            stripped_pos = token['pos'].split('^', 1)[0]
+            if '+' in stripped_pos:
+                stripped_pos = stripped_pos.split('+', 1)[1]
+            if stripped_pos in stress_words:
+                token['lift'] = True
                 formatted.append(token['form'].upper())
+            # also the last word in the offverse ?nearly always accommodates a lift:
+            elif halfline == 'b':
+                # TODO: not working. mark the final word of the off-verse as a lift:
+                if idx == len(line[halfline]) - 1:
+                    token['lift'] = True
+                    formatted.append(token['form'].upper())
+                else:
+                    formatted.append(token['form'])
             else:
                 formatted.append(token['form'])
+            data.append(token)
 
-        if sum(1 for token in formatted if token.isupper()) > 3:
-            print(f"{str("{:04d}".format(int(line['a'][0]['verse'].rstrip('ax'))))} {' '.join(formatted)}")
+    formatted = str("{:04d}".format(int(line_no))) + ' ' + ' '.join(formatted)
+
+    return data,formatted
 
 if not(c_json_file.is_file()):
     helipad_extract.extract()
 with open(c_json_file) as c_json_data:
     c_tokens = json.load(c_json_data)
-c_verses_file = Path('heliand-c-verses.json')
 if not(c_verses_file.is_file()):
     c_verses = dict.fromkeys(range(1, c_range))
     for i in range(1, c_range):
@@ -116,5 +153,16 @@ else:
     with open(c_verses_file) as f:
         c_verses = json.load(f)
 
+new_db = []
+plaintext = ''
 for i in range(1, c_range):
-    scan(c_verses[str(i)])
+    findings,plaintext_line = scan(c_verses[str(i)])
+    if findings is not None:
+        new_db.extend(findings)
+        plaintext = plaintext + plaintext_line + '\n'
+
+with open(json_out, 'w') as f:
+    json.dump(new_db, f, ensure_ascii=False, indent=4)
+
+with open(plaintext_out, 'w') as f:
+    f.write(plaintext)
